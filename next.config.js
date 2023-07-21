2a796156dd451c10ba8def58208a39e692d24030
mezhendosina <mensh.zheya@gmail.com>
2023-07-21 17:26:09 +0500

    edit nextConfig

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    distDir: '_static',
-    images: {
-        unoptimized: true
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
